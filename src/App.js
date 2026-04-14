a01d593fb03d97300dc2bccfd592860c62beca68
psihimanshuyadav <93133955+psihimanshuyadav@users.noreply.github.com>
2026-04-14 17:20:57 +0530

    Fix formatting issues in App.js

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ function App() {
         >
           Learn React
         </a>
+             <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+            
         <a
           className="App-link"
           href="https://reactjs.org"
