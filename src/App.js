bf1dc52d4d5f379c9df0caa6071ea50da47839d6
Himanshu Yadav <himanshu.yadav@thepsi.com>
2026-04-15 11:18:01 +0530

    4th Commit

--- src/App.js
+++ src/App.js
@@ -19,14 +19,7 @@ function App() {
         </a>
             
             
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        
          <a
           className="App-link"
           href="https://reactjs.org"
